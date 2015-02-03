712224aa7606a34548e8e41d251af00ecf33d20d
Rishya <sithiravel3@gmail.com>
2015-02-03 13:44:06 -0500

    Added looping structure and added array stuf

--- COMP123- Lesson05/Program.cs
+++ COMP123- Lesson05/Program.cs
@@ -10,6 +10,62 @@ namespace COMP123__Lesson05
     {
         static void Main(string[] args)
         {
+            
+            string[] carMakeList = {"Honda","BMW", "Mercedes", "Jeep", "Ford"} ;
+            string[] tempCarMakeList = new string[3];
+            
+            Random rnd = new Random();
+            int randomCar;
+            randomCar = rnd.Next(6);
+            Console.WriteLine("My random car is {0}", randomCar);
+
+
+            for (int index = 0; index < tempCarMakeList.Length; index++)
+            {
+                tempCarMakeList[index] = carMakeList[index];
+            }
+
+
+           
+
+
+                /* Alternate way to declare and initialize an array string[] carMakeList = new string[6]; 
+              string[] carMakeList = new string[6];
+
+              carMakeList[0] = "Honda";
+              carMakeList[1] = "BMW";
+              carMakeList[2] = "Mercedes";
+              carMakeList[3] = "Jeep";
+              carMakeList[4] = "Ford";
+              carMakeList[5] = "Jaguar";
+               * */
+
+
+
+                for (int index = 0; index < carMakeList.Length; index++)
+                {
+                    Console.WriteLine(carMakeList[index]);
+                   
+                }
+               
+
+
+
+
+                    /*Alternate method to loop through(iterate acros the aray)
+                    int index = 0;
+                    while(index< carMakeList.Length)
+                    {
+                        Console.WriteLine(carMakeList[index]);
+                        index++;
+                    }
+                     * */
+
+                    Console.WriteLine("");
+            Console.WriteLine("++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
+
     }
 }
